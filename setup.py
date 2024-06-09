65a944bbc29efdad4f3c4938ba5270c9df291e51
Carlos Adames <adames@ouroanalytics.ai>
2024-06-09 16:49:39 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.11',
     install_requires=[
         'pandas>=2.2.2',
         'matplotlib>=3.9.0'
