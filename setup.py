e4935ea09f1d721f5bdfb228db1fd5b396c9c26d
Carlos Adames <adames@ouroanalytics.ai>
2024-06-09 16:44:02 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pybiplot",
-    version="0.0.3",
+    version="0.0.4",
     author="Carlos A. Adames Ramos",
     author_email="carlos.adames.ramos@gmail.com",
     description="R's BiPlot visual for PCA in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/adames-ouro/PyBiPlot",
+    url="https://github.com/adames-ouro/pybiplot",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.12',
     install_requires=[
         'pandas>=2.2.2',
         'matplotlib>=3.9.0'
